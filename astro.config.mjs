a2433bca5e0eca0d8799caaa8a58ce6ff77e5961
Chubby Granny Chaser <hydralauncher@proton.me>
2026-04-30 11:52:17 +0100

    Simplify deployment and remove obsolete screenshots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   server: {
-    host: true,
+    host: "0.0.0.0",
   },
   integrations: [tailwind({ applyBaseStyles: false }), react()],
-  adapter: node({
-    mode: "standalone",
-  }),
 });
